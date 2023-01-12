94b247ca3e02d8c7583d8db7249c6252ba07748f
Marlen Temirbaev <mouflon150@gmail.com>
2023-01-12 17:02:39 +0600

    Commit Message

--- src/DatabaseFunctions.sql
+++ src/DatabaseFunctions.sql
@@ -1,26 +1,45 @@
 --- DDL (Data Definition Language)
 --- CREATE TABLE
+--- INSERT
 --- ALTER TABLE
 --- DROP TABLE
-CREATE TABLE pupils
+CREATE TABLE students
 (
-    pupil_id SMALLINT PRIMARY KEY ,
+    student_id SMALLINT PRIMARY KEY ,
     first_name VARCHAR(20) NOT NULL ,
     last_name VARCHAR(30) NOT NULL,
-    gender CHARACTER NOT NULL ,
-    phone_or_email VARCHAR(100) ,
+    gender CHARACTER NOT NULL CHECK ( gender = 'M' OR gender = 'F' ),
+    phone_or_email VARCHAR(100) UNIQUE ,
     typing VARCHAR(10) NOT NULL ,
     address TEXT NOT NULL
 );
 
+DROP TABLE students;
+
+ALTER TABLE students
+RENAME TO pupils;
+
+ALTER TABLE students
+ADD COLUMN birth DATE;
+
+INSERT INTO students(student_id, first_name, last_name, gender, birth, phone_or_email, typing, address)
+VALUES
+(1, 'Marlen', 'Temirbaev', 'M', '2005-04-27', 'mouflon150@gmail.com', '50wpm', 'Zavodskaya street № ?'),
+(2, 'Dastan', 'Abdullaev', 'M', '2006-02-04', 'dosyafree32@gmail.com', '50wpm', 'unknown'),
+(3, 'Bekbolsun', 'Nasipbekov', 'M', '2006-03-12', 'nasipbekov01@gmail.com', '40wpm', 'unknown'),
+(4, 'Daniel', 'Ahadzanov', 'M', '2007-11-11', 'danijelahatzanov@gmail.com', '90wpm', 'unknown'),
+(5, 'Sardor', 'Kalmurzaev', 'M', '2005-09-30', '+996771007806', '40wpm', 'Kolhoznaya street № ?'),
+(6, 'Elbek', 'Mashrapov', 'M', '2006-08-02', 'elbekmashrapov@gmail.com', '90wpm', 'unknown');
+
+SELECT *
+FROM students;
+
 --- DML (Data Manipulating Language)
---- INSERT
 --- UPDATE
 --- DELETE
-INSERT INTO pupils(pupil_id, first_name, last_name, gender, phone_or_email, typing, address)
-VALUES
-(1, 'Marlen', 'Temirbaev', 'M', 'mouflon150@gmail.com', '50wpm', 'Zavodskaya street № ?'),
-(2, 'Dastan', 'Abdullaev', 'M', 'dosyafree32@gmail.com', '50wpm', 'unknown'),
-(3, 'Bekbolsun', 'Nasipbekov', 'M', 'nasipbekov01@gmail.com', '40wpm', 'unknown'),
-(4, 'Daniel', 'Ahadzanov', 'M', 'danijelahatzanov@gmail.com', '90wpm', 'near an educational institution'),
-(5, 'Sardor', 'Kalmurzaev', 'M', '+996771007806', '40wpm', 'Kolhoznaya street № ?');
+UPDATE students
+SET phone_or_email = 'neon30@gmail.com'
+WHERE phone_or_email = '+996771007806';
+
+DELETE FROM students
+WHERE student_id = 6;
